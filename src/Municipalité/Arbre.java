53ced4d79b8be0cfa4a3e5677fecd4241b2ad63f
LeoLLob <leo.lloberas@gmail.com>
2021-05-28 15:32:34 +0200

    variables arbre

--- src/Municipalité/Arbre.java
+++ src/Municipalité/Arbre.java
@@ -1,14 +1,28 @@
 package Municipalité;
 
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.util.List;
 
 public class Arbre{
 
-    public final static char SEPARATOR = ';';
-
-    private File file;
-    private List<String> line;
+    private int idbase;
+    private String typeemplacement;
+    private String domanialite;
+    private String arrondissement;
+    private String complementadresse;
+    private int numero;
+    private String adresse;
+    private String idemplacement;
+    private String libellefrancais;
+    private String genre;
+    private String espece;
+    private String varieteoucultivar;
+    private int circonferenceencm;
+    private int hauteurenm;
+    private String stadedeveloppement;
+    private boolean remarquable;
+    private Point2D geo_point_2d;
 
 
 
