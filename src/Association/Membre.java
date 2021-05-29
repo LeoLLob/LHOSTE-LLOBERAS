9d3274de59019efdf35b77f72bd6829a16351517
LeoLLob <leo.lloberas@gmail.com>
2021-05-29 10:43:35 +0200

    création fonctions csv

--- src/Association/Membre.java
+++ src/Association/Membre.java
@@ -1,4 +1,17 @@
 package Association;
 
+import Municipalité.Arbre;
+
 public class Membre extends Association{
+    private String nom;
+    private String prenom;
+    private String dateDeNaissance;
+    private String adresse;
+    private String dateDePremiereInscription;
+    private String[] cotisation;
+    private boolean aPaye;
+    private boolean estInscrit;
+    private Arbre[] listeArbre;
+
+
 }
--- src/Municipalité/Arbre.java
+++ src/Municipalité/Arbre.java
@@ -1,8 +1,8 @@
 package Municipalité;
 
 import java.awt.geom.Point2D;
-import java.io.File;
-import java.util.List;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Arbre{
 
@@ -11,19 +11,66 @@ public class Arbre{
     private String domanialite;
     private String arrondissement;
     private String complementadresse;
-    private int numero;
+    private String numero;
     private String adresse;
     private String idemplacement;
     private String libellefrancais;
     private String genre;
     private String espece;
     private String varieteoucultivar;
-    private int circonferenceencm;
-    private int hauteurenm;
+    private String circonferenceencm;
+    private String hauteurenm;
     private String stadedeveloppement;
-    private boolean remarquable;
+    private String remarquable;
     private Point2D geo_point_2d;
 
+    public static ArrayList<String[]> getArbres(String path){
+
+        String line = "";
+        String[] arbre = {""};
+        ArrayList<String[] > arbres = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            while((line = bufferedReader.readLine()) != null){
+                arbre = line.split(";");
+                arbres.add(arbre);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arbres;
+    }
+
+
+    public static ArrayList<String[]> getRemarquable(ArrayList<String[]> arbres){
+
+        ArrayList<String[] > arbresRemarquables = new ArrayList<>();
+        for(String[] line: arbres){
+            if(line[15] == ""){
+                arbresRemarquables.add(line);
+            }
+        }
+        return arbresRemarquables;
+    }
+
+
+
+
+    public static void main(String[] args){
+        Arbre arbre = new Arbre();
+        ArrayList<String[]>  arbres = Arbre.getArbres("les-arbres.csv");
+        ArrayList<String[]>  arbresRemarquables = Arbre.getRemarquable(arbres);
+        for(String[] line: arbresRemarquables){
+            for(String string:line){
+                System.out.print(string + "; ");
+            }
+        System.out.println();
+
+        }
+
+    }
 
 
 }
